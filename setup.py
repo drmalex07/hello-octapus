68c62001bd17bc7d40a19f427ca2019246b2f0e0
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-05-18 13:52:39 +0300

    Added setup files for Python packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.2a'
+
+setup(name='hello-octapus',
+      version=version,
+      description="Notin",
+      long_description="""""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='nothingness helloworld',
+      author='Michail Alexakis',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'nose',
+          'autopep8',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
