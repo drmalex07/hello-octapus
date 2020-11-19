582db4286b432f32e0a11c45685e59a8355dbbbb
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2020-11-19 13:14:52 +0200

    Update .drone.yml

--- echo-secrets.sh
+++ echo-secrets.sh
@@ -4,3 +4,4 @@ echo DB_USER=${DB_USER}
 echo DB_PASS=${DB_PASS}
 echo DB_PASS=$(echo ${DB_PASS} | tr '[[:lower:]]' '[[:upper:]]')
 
+echo REGISTRY_PASSWORD=$(echo -n "<<${REGISTRY_PASSWORD}>>" | tr '[[:lower:]]' '[[:upper:]]')
